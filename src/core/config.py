ab61659517f6b056bc33d64316f9a6e98f3feac4
Rinalon <mit.krist@yandex.ru>
2026-07-16 16:38:28 +0300

    fix: correct Settings in cofing.py

--- src/core/config.py
+++ src/core/config.py
@@ -6,24 +6,24 @@ class Settings(BaseSettings):
     DB_HOST: str
     DB_PORT: int
     DB_USER: str
-    DB_PASSWORD: str
+    DB_PASS: str
     DB_NAME: str
     DEBUG: bool = True
 
     model_config = SettingsConfigDict(
-        env_file=Path(__file__).parent.parent / ".env",
+        env_file=Path(__file__).parent.parent.parent / ".env",
     )
 
     @property
     def database_url_async(self) -> str:
         user = quote_plus(self.DB_USER)
-        password = quote_plus(self.DB_PASSWORD)
+        password = quote_plus(self.DB_PASS)
         return f"postgresql+asyncpg://{user}:{password}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
     @property
     def database_url(self) -> str:
         user = quote_plus(self.DB_USER)
-        password = quote_plus(self.DB_PASSWORD)
+        password = quote_plus(self.DB_PASS)
         return f"postgresql://{user}:{password}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
 settings = Settings()
