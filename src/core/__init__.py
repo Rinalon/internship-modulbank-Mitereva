016e540b7e043c94da6aee3f50beacfbcb87b1b5
Rinalon <mit.krist@yandex.ru>
2026-07-16 17:50:36 +0300

    feat: create engines in database.py

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,5 +1,8 @@
 from src.core.config import settings
+from src.core.database import engine, async_engine
 
 __all__ = [
     "settings",
+    "engine",
+    "async_engine",
 ]
---
+++ src/core/database.py
@@ -0,0 +1,29 @@
+from src.core import settings
+from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+
+engine = create_engine(
+    url=settings.database_url_psycopg,
+    echo=settings.DEBUG,
+    #pool_size=5,
+    #max_overflow=2
+)
+
+
+async_engine = create_async_engine(
+    url=settings.database_url_async,
+    echo=settings.DEBUG,
+    #pool_size=5,
+    #max_overflow=2
+)
+
+AsyncSessionLocal = async_sessionmaker(
+    async_engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
+
+async def get_db() -> AsyncSession:
+    """Генератор сессий для внедрения в эндпоинты FastAPI."""
+    async with AsyncSessionLocal() as session:
+        yield session
